73a6d579addcc05b730ca5276d5fd80543044bda
Lidia <lidia.parrilla@idener.es>
2021-10-27 17:14:40 +0200

    Adding send SMS capability. To be tested

--- main.ino
+++ main.ino
@@ -25,7 +25,15 @@
 
 	// Input button
 	#define PIN_INPUT_BUTTON 7
+	
+	// Serial TX and RX for communication with SIM800L
+	// TODO: Check NANO
+	#define PIN_RX 2
+	#define PIN_TX 3
   
+// Create object for serial communication with SIM800
+SoftwareSerial SerialSIM800(PIN_RX, PIN_TX);
+
 // ------------ Constants definition ------------//
 	// Minimum time between consecutive irrigations in milliseconds
 	const int TIME_BETWEEN_IRRIGATIONS = 2000;
@@ -44,6 +52,9 @@
 	
 	// Voltage threshold for irrigation (only irrigate over this value)
 	const int VOLTAGE_THRESHOLD = 6;
+	
+	// Phone number for the SMSs
+	const String PHONE_NUMBER = "+34605521505";
 
 // ------------ Global variables ------------//
 	// Boolean for the irrigation valve to be open
@@ -82,12 +93,28 @@ void setup()
 
 	// Store the initial time
 	prevFlowMeasurementTime = millis();
+	
+	// Initialise serial communication with SIM800 for AT commands	
+	SerialSIM800.begin(9600);
+	
+	// DEBUGGING - SMS TEST
+	sendSMS("Hello from SIM800L", PHONE_NUMBER);
 }
 
 void loop() 
 {
 	// Time measurement
 	long currentMillis = millis( ) ;
+	
+	// Check if anything has been received in the SIM800
+	if (SerialSIM800.available()){            
+		if(DEBUG_MODE) 
+		{
+			//Displays on the serial monitor if there's a communication from the module
+			Serial.println("Communication received from SIM800: ");
+			Serial.write(SerialSIM800.read()); 
+		}
+	}
 	
 	// Read sensors
 	int humidity = analogRead(PIN_HUMIDITY_SENSOR);	
@@ -212,4 +239,29 @@ void printMeasurements(int hum, int temp, float voltage, float waterFlow, float 
 	Serial.println (" L");
 	Serial.print("Boton pulsado: ");
 	Serial.println(buttonPressed);
+}
+
+void sendSMS(String text, String phone_number)
+{
+	if(DEBUG_MODE) Serial.println("Sending SMS..."); //Show this message on serial monitor
+	//Set the module to SMS mode
+	SerialSIM800.print("AT+CMGF=1\r");                   
+	delay(100);
+	
+	//Your phone number don't forget to include your country code, example +212123456789"
+	SerialSIM800.print("AT+CMGS=\""+phone_number+"\"\r");  
+	delay(500);
+	
+	//This is the text to send
+	SerialSIM800.print(text);       
+	delay(500);
+	
+	//(required according to the datasheet)
+	SerialSIM800.print((char)26);
+	delay(500);
+	
+	SerialSIM800.println();
+	
+	if(DEBUG_MODE) Serial.println("Text Sent.");
+	delay(500);
 }
---
+++ test/SIM800_SMS_test.ino
@@ -0,0 +1,64 @@
+#include <SoftwareSerial.h> 
+
+// Debug mode configuration
+# define DEBUG_MODE true
+
+// Serial TX and RX for communication with SIM800L
+// TODO: Check NANO
+#define PIN_RX 2
+#define PIN_TX 3
+
+// Create object for serial communication with SIM800
+SoftwareSerial SerialSIM800(PIN_RX, PIN_TX);
+
+// Phone number for the SMSs
+const String PHONE_NUMBER = "+34605521505";
+
+void setup() 
+{
+	// Start serial connection 
+	if(DEBUG_MODE) Serial.begin(9600); Serial.println ("Enviar 'r' para restablecer el volumen a 0 Litros"); 
+	
+	// Initialise serial communication with SIM800 for AT commands	
+	SerialSIM800.begin(9600);
+	
+	// DEBUGGING - SMS TEST
+	sendSMS("Hello from SIM800L", PHONE_NUMBER);
+}
+
+void loop()
+{
+	if (SerialSIM800.available()){            
+		if(DEBUG_MODE) 
+		{
+			//Displays on the serial monitor if there's a communication from the module
+			Serial.println("Communication received from SIM800: ");
+			Serial.write(SerialSIM800.read()); 
+		}
+	}
+}
+
+void sendSMS(String text, String phone_number)
+{
+	if(DEBUG_MODE) Serial.println("Sending SMS..."); //Show this message on serial monitor
+	//Set the module to SMS mode
+	SerialSIM800.print("AT+CMGF=1\r");                   
+	delay(100);
+	
+	//Your phone number don't forget to include your country code, example +212123456789"
+	SerialSIM800.print("AT+CMGS=\""+phone_number+"\"\r");  
+	delay(500);
+	
+	//This is the text to send
+	SerialSIM800.print(text);       
+	delay(500);
+	
+	//(required according to the datasheet)
+	SerialSIM800.print((char)26);
+	delay(500);
+	
+	SerialSIM800.println();
+	
+	if(DEBUG_MODE) Serial.println("Text Sent.");
+	delay(500);
+}
