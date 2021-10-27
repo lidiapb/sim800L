13303984ee603dd964f259bf9d687d85879aa6fc
Lidia <lidia.parrilla@idener.es>
2021-10-27 16:47:42 +0200

    Initial cleanup

--- main.ino
+++ main.ino
@@ -1,165 +1,215 @@
-  #include <SoftwareSerial.h> 
+#include <SoftwareSerial.h> 
 
-  //valvula Riego
-  const int ini_1 = 12;
-  const int ini_2 = 11;
+// ------------ Debug mode configuration ------------//
+	# define DEBUG_MODE true
 
-  int abrir = 0;
-  int checkCerrar = 0;
-  int desactivacionTotal = 0;
+// ------------ Pins definition ------------//
+	// Valve: 2 pins for H-bridge (A-, A+)
+	#define PIN_VALVE_A1 12
+	#define PIN_VALVE_A2 11
 
-  int timeSalidaRiego = 2000; // tiempo minimo entre riegos
-  unsigned long PreviousMillis = 0 ;
-  const long interval = 7000 ; // tiempo de riego eficaz
+	// Water flow sensor
+	#define PIN_FLOW_SENSOR 2
+	
+	// Voltage sensor
+	#define PIN_VOLTAGE_SENSOR A0
+	
+	// Humidity sensor
+	#define PIN_HUMIDITY_SENSOR A1
+	
+	// Temperature sensor
+	#define PIN_TEMPERATURE_SENSOR A2
 
-  volatile int NumPulsos; //variable para la cantidad de pulsos recibidos
-  int PinSensor = 2;    //Sensor flujo conectado en el pin 2
-  float factor_conversion=7.11; //para convertir de frecuencia a caudal
-  float volumen=0;
-  long dt=0; //variación de tiempo por cada bucle
-  long t0=0; //millis() del bucle anterior
+	// Safety relay
+	#define PIN_SAFETY_RELAY 10
 
-  //---Función que se ejecuta en interrupción---------------
-  void ContarPulsos ()  
-{ 
-  NumPulsos++;  //incrementamos la variable de pulsos
+	// Input button
+	#define PIN_INPUT_BUTTON 7
   
+// ------------ Constants definition ------------//
+	// Minimum time between consecutive irrigations in milliseconds
+	const int TIME_BETWEEN_IRRIGATIONS = 2000;
+
+	// Effective irrigation time in milliseconds
+	const int EFFECTIVE_IRRIGATION_TIME = 7000;
+
+	// Conversion factor from Hz to L/min flow for the water flow sensor
+	const float FLOW_CONVERSION_FACTOR = 7.11;
+	
+	// Humidity threshold for irrigation (only irrigate below this value)
+	const int HUMIDITY_THRESHOLD = 720;
+	
+	// Temperature threshold for irrigation (only irrigate over this value)
+	const int TEMPERATURE_THRESHOLD = 980;
+	
+	// Voltage threshold for irrigation (only irrigate over this value)
+	const int VOLTAGE_THRESHOLD = 6;
+
+// ------------ Global variables ------------//
+	// Boolean for the irrigation valve to be open
+	bool valveOpen = false;
+	
+	// Boolean for relay status
+	bool relayOn = false;
+
+    //Reference time for previous water volume update
+	long prevFlowMeasurementTime = 0; 
+	
+	// Reference time when the irrigation started
+	unsigned long irrigationStartTime = 0 ;
+
+	// Counter for flow sensor pulses
+	volatile int pulsesCount; 
+	
+	// Storage of total water volume since the program start
+	float totalWaterVolume = 0;
+		
+
+void setup() 
+{
+	// Start serial connection 
+	if(DEBUG_MODE) Serial.begin(9600); Serial.println ("Enviar 'r' para restablecer el volumen a 0 Litros"); 
+  
+	// Define pin modes
+	pinMode(PIN_FLOW_SENSOR, INPUT); 
+	pinMode(PIN_INPUT_BUTTON, INPUT_PULLUP); // Configura pin 7 interno pull-up resistor  Estado en 1, requiere 0 para activarse
+	pinMode(PIN_VALVE_A1, OUTPUT); // Pin valvula A-
+	pinMode(PIN_VALVE_A2, OUTPUT); // Pin valvula A+
+	pinMode(PIN_SAFETY_RELAY, OUTPUT); // Salida rele para apagado voltage seguridad
+
+	// Define interruption for flow sensor
+	attachInterrupt(0, countPulses ,RISING);//(Interrupción 0(Pin2),función,Flanco de subida)
+
+	// Store the initial time
+	prevFlowMeasurementTime = millis();
+}
+
+void loop() 
+{
+	// Time measurement
+	long currentMillis = millis( ) ;
+	
+	// Read sensors
+	int humidity = analogRead(PIN_HUMIDITY_SENSOR);	
+	int temperature = analogRead(PIN_TEMPERATURE_SENSOR);
+	float voltage = analogRead(PIN_VOLTAGE_SENSOR)* (5.01 / 1023.00) * 1.24;
+	float water_flow_L_min = measureWaterFlow();
+	
+	// Calculate water volume and sum to previous one
+	int dt = currentMillis - prevFlowMeasurementTime; //calculamos la variación de tiempo
+	prevFlowMeasurementTime = currentMillis;
+	
+	// Read serial input to flush volume storage
+	if (Serial.available() && Serial.read()=='r') totalWaterVolume = 0;//restablece el volumen si recibe 'r'  
+	else totalWaterVolume += (water_flow_L_min/60)*(dt/1000); // volumen(L)=caudal(L/s)*tiempo(s)
+		
+	// Check button status. If it is low, buttonPresed = true
+	bool buttonPressed = !digitalRead(PIN_INPUT_BUTTON);	
+	
+	// Print measurements only in debug mode
+	if(DEBUG_MODE) printMeasurements(humidity, float temp, float voltage, float waterFlow, float totalVolume, bool buttonPressed);
+  
+	// If batteries are running out, temperature is too low or humidity is too high, turn off the system for safety and to save power
+	if(humidity > HUMIDITY_THRESHOLD || voltage < VOLTAGE_THRESHOLD || temperature < TEMPERATURE_THRESHOLD)
+	{
+		// Ensure valves stay closed
+		closeValve();
+		valveOpen = false;
+		
+		if(relayOn)
+		{
+			// Turn off relay to remove power from the valve
+			digitalWrite(PIN_SAFETY_RELAY, LOW);
+			
+			relayOn = false;
+		}
+	}
+	else
+	{
+		if(!relayOn)
+		{
+			// Turn on relay to give power to the valve if it was off
+			digitalWrite(PIN_SAFETY_RELAY, HIGH);
+			
+			relayOn = true;
+		}
+		
+		// If irrigation is requested
+		if (buttonPressed) 
+		{
+			// Only irrigate if enough time has passed since last irrigation
+			// Only irrigate if enough time has passed since last irrigation
+			if ((currentMillis - irrigationStartTime) >= (EFFECTIVE_IRRIGATION_TIME + TIME_BETWEEN_IRRIGATIONS) && !valveOpen) 
+			{
+				irrigationStartTime = currentMillis;
+				valveOpen = true;
+				openValve();
+			}
+		}
+		
+		// If the valve has been open for the configured EFFECTIVE_IRRIGATION_TIME, turn it off
+		if (currentMillis - irrigationStartTime >= EFFECTIVE_IRRIGATION_TIME && valveOpen) 
+		{
+			valveOpen = false;
+			closeValve();
+		}
+	}
+	delay(100);
+}
+
+// -------- Function to open valve ---------- //
+void openValve() 
+{
+	digitalWrite(PIN_VALVE_A1, HIGH);
+	digitalWrite(PIN_VALVE_A2, LOW);
+	delay(300);
+	digitalWrite(PIN_VALVE_A1, LOW);
+	if(DEBUG_MODE) Serial.println("OPEN RIEGO");
+}
+
+// -------- Function to close valve ---------- //
+void closeValve() 
+{
+	digitalWrite(PIN_VALVE_A2, HIGH);
+	digitalWrite(PIN_VALVE_A1, LOW);
+	delay(300);
+	digitalWrite(PIN_VALVE_A2, LOW);
+	if(DEBUG_MODE) Serial.println("CLOSED RIEGO");
+}
+
+//---Función que se ejecuta en interrupción (ISR) ---------------//
+void countPulses ()  
+{ 
+	pulsesCount++;  //incrementamos la variable de pulsos 
 } 
 
-  //---Función para obtener frecuencia de los pulsos--------
-  int ObtenerFrecuecia() 
+//---Función para obtener frecuencia de los pulsos--------//
+float measureWaterFlow() 
 {
-  int frecuencia;
-  NumPulsos = 0;   //Ponemos a 0 el número de pulsos
-  interrupts();    //Habilitamos las interrupciones
-  delay(1000);   //muestra de 1 segundo
-  noInterrupts(); //Deshabilitamos  las interrupciones
-  frecuencia=NumPulsos; //Hz(pulsos por segundo)
-  return frecuencia;
-  
+	pulsesCount = 0; //Ponemos a 0 el número de pulsos
+	interrupts();    //Habilitamos las interrupciones
+	delay(1000);   //muestra de 1 segundo
+	noInterrupts(); //Deshabilitamos  las interrupciones
+	
+	float waterFlow = pulsesCount/FLOW_CONVERSION_FACTOR
+	return waterFlow;
 }
 
-  void setup() {
-
-  //start serial connection
-  
-  Serial.begin(9600);
-  pinMode(PinSensor, INPUT); 
-  attachInterrupt(0,ContarPulsos,RISING);//(Interrupción 0(Pin2),función,Flanco de subida)
-  Serial.println ("Enviar 'r' para restablecer el volumen a 0 Litros"); 
-  t0=millis();
-  //configura pin 7 interno pull-up resistor
-  pinMode(7, INPUT_PULLUP); // Estado en 1, requiere 0
-  para activarse
-  pinMode(ini_1, OUTPUT); // Pin valvula A-
-  pinMode(ini_2, OUTPUT); // Pin valvula A+
-  pinMode(10, OUTPUT); // Salida rele para apagado voltage seguridad
-
-}
-
-  void loop() {
-
-  int sensorVal = digitalRead(7);
-  int sensorValue = analogRead(A0);
-  float voltage = sensorValue * (5.01 / 1023.00) * 1.24;
-  int  humedad = analogRead(A1);
-  int paramHumedad = 0;
-  int temperatura = analogRead(A2);
-  int paramTemperatura = 0;
-  long currentMillis = millis ( ) ;
-
-  if (Serial.available()) {
-  if(Serial.read()=='r')volumen=0;//restablece el volumen si recibe 'r'
-  
-}
-
-  float frecuencia=ObtenerFrecuecia(); //obtenemos frecuencia de los pulsos en Hz
-  float caudal_L_m=frecuencia/factor_conversion; //calculamos el caudal en L/m
-  dt=millis()-t0; //calculamos la variación de tiempo
-  t0=millis();
-  volumen=volumen+(caudal_L_m/60)*(dt/1000); // volumen(L)=caudal(L/s)*tiempo(s)
-
-  Serial.print("humedad--");
-  Serial.println(humedad);
-  Serial.print("temperatura--");
-  Serial.println(temperatura);
-  Serial.println(sensorVal);
-  Serial.println(voltage);
-  Serial.print ("Caudal: "); 
-  Serial.print (caudal_L_m,3); 
-  Serial.print ("L/min\tVolumen: "); 
-  Serial.print (volumen,3); 
-  Serial.println (" L");
-  
-  if (humedad > 720 || voltage < 6 || temperatura < 980) {
-  paramHumedad = 1;
-  paramTemperatura = 1;
-    
-  // Elegir alarmas con desactivacion total:
-    
-  if (humedad > 720 || voltage < 6 || temperatura < 980) {
-  if(desactivacionTotal==0){
-  desactivacionTotal=1;
-  cerrar_valvula();
-  checkCerrar = 0;
-  abrir = 0;
-  digitalWrite(10, LOW);
-}
-} else {
-  
-  if ( currentMillis - PreviousMillis >= interval && abrir == 1 && checkCerrar == 0 ) {
-  checkCerrar = 1;
-  cerrar_valvula();
-}
-       
-  if ( currentMillis - PreviousMillis >= (interval + timeSalidaRiego) && abrir == 1 ) {
-  checkCerrar = 0;
-  abrir = 0;
-}
-}
-} else {
-  paramHumedad = 0;
-  desactivacionTotal =0;
-  paramTemperatura = 0;
-  digitalWrite(10, HIGH);
-}
-  if (paramHumedad == 0) {
-  if (sensorVal == LOW) {
-
-  //Riego intervalo minimo "x" segundos
-  if ( currentMillis - PreviousMillis >= interval && abrir == 0 ) {
-  PreviousMillis = currentMillis ;
-  abrir = 1;
-  abre_valvula();
-}
-}
-  if ( currentMillis - PreviousMillis >= interval && abrir == 1 && checkCerrar == 0 ) {
-  checkCerrar = 1;
-  cerrar_valvula();
-}
-
-  if ( currentMillis - PreviousMillis >= (interval + timeSalidaRiego) && abrir == 1 ) {
-  checkCerrar = 0;
-  abrir = 0;
-}
-}
-
-  delay(100);
- 
-}
-
-  void abre_valvula() {
-  digitalWrite(ini_1, HIGH);
-  digitalWrite(ini_2, LOW);
-  delay(300);
-  digitalWrite(ini_1, LOW);
-  Serial.println("OPEN RIEGO");
-}
-
-  void cerrar_valvula() {
-  digitalWrite(ini_2, HIGH);
-  digitalWrite(ini_1, LOW);
-  delay(300);
-  digitalWrite(ini_2, LOW);
-  Serial.println("CLOSED RIEGO");
+//--- Function for pretty-printing the measurements--------//
+void printMeasurements(int hum, int temp, float voltage, float waterFlow, float totalVolume, bool buttonPressed)
+{
+	if(!DEBUG_MODE) return;
+	Serial.print("Humedad: ");
+	Serial.println(hum);
+	Serial.print("Temperatura: ");
+	Serial.println(temp);
+	Serial.print("Voltage: ");
+	Serial.println(voltage);
+	Serial.print ("Caudal: "); 
+	Serial.print (waterFlow,3); 
+	Serial.print ("L/min\tVolumen: "); 
+	Serial.print (totalVolume,3); 
+	Serial.println (" L");
+	Serial.print("Boton pulsado: ");
+	Serial.println(buttonPressed);
 }
