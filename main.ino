9be9ca7609b04ee3529fe5962d5f6a69b28659bc
Lidia <lidia.parrilla@idener.es>
2021-10-27 17:26:28 +0200

    Removed flush volume and added function to print received message from module

--- main.ino
+++ main.ino
@@ -28,8 +28,8 @@
 	
 	// Serial TX and RX for communication with SIM800L
 	// TODO: Check NANO
-	#define PIN_RX 2
-	#define PIN_TX 3
+	#define PIN_RX 0
+	#define PIN_TX 1
   
 // Create object for serial communication with SIM800
 SoftwareSerial SerialSIM800(PIN_RX, PIN_TX);
@@ -105,16 +105,6 @@ void loop()
 {
 	// Time measurement
 	long currentMillis = millis( ) ;
-	
-	// Check if anything has been received in the SIM800
-	if (SerialSIM800.available()){            
-		if(DEBUG_MODE) 
-		{
-			//Displays on the serial monitor if there's a communication from the module
-			Serial.println("Communication received from SIM800: ");
-			Serial.write(SerialSIM800.read()); 
-		}
-	}
 	
 	// Read sensors
 	int humidity = analogRead(PIN_HUMIDITY_SENSOR);	
@@ -263,8 +253,15 @@ void sendSMS(String text, String phone_number)
 	delay(500);
 }
 
-void serialEvent(){
-	// Read serial input to flush volume storage
-	if (Serial.available() && Serial.read()=='r') totalWaterVolume = 0;//restablece el volumen si recibe 'r' 
-	prevFlowMeasurementTime = millis();
+// Serial event on monitor serial1
+void serialEvent(){   
+	// Check if anything has been received in the SIM800
+	if (SerialSIM800.available() > 0){            
+		if(DEBUG_MODE) 
+		{
+			//Displays on the serial monitor if there's a communication from the module
+			Serial.println("Message received from SIM800: ");
+			Serial.write(SerialSIM800.readStringUntil("\n")); 
+		}
+	}
 }	
