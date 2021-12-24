b7605732ef4cd7ec733f36711624bb570e342da1
Lidia <lidia.parrilla@idener.es>
2021-12-24 18:26:25 +0100

    Moved changes regarding SMS to main file after test

--- main.ino
+++ main.ino
@@ -27,9 +27,8 @@
 	#define PIN_INPUT_BUTTON 7
 	
 	// Serial TX and RX for communication with SIM800L
-	// TODO: Check NANO
-	#define PIN_RX 0
-	#define PIN_TX 1
+	#define PIN_RX 8
+	#define PIN_TX 9
   
 // Create object for serial communication with SIM800
 SoftwareSerial SerialSIM800(PIN_RX, PIN_TX);
@@ -98,13 +97,23 @@ void setup()
 	SerialSIM800.begin(9600);
 	
 	// DEBUGGING - SMS TEST
-	sendSMS("Hello from SIM800L", PHONE_NUMBER);
+	//sendSMS("Hello from SIM800L", PHONE_NUMBER);
 }
 
 void loop() 
 {
 	// Time measurement
 	long currentMillis = millis( ) ;
+	
+	// Check if anything has been received in the SIM800
+	if (SerialSIM800.available()){            
+		if(DEBUG_MODE) 
+		{
+			//Displays on the serial monitor if there's a communication from the module
+			Serial.println("Communication received from SIM800: ");
+			Serial.write(SerialSIM800.read()); 
+		}
+	}
 	
 	// Read sensors
 	int humidity = analogRead(PIN_HUMIDITY_SENSOR);	
@@ -115,7 +124,10 @@ void loop()
 	// Calculate water volume and sum to previous one
 	int dt = currentMillis - prevFlowMeasurementTime; //calculamos la variación de tiempo
 	prevFlowMeasurementTime = currentMillis;
-	totalWaterVolume += (waterFlow_L_min/60)*(dt/1000); // volumen(L)=caudal(L/s)*tiempo(s)
+	
+	// Read serial input to flush volume storage
+	if (Serial.available() && Serial.read()=='r') totalWaterVolume = 0;//restablece el volumen si recibe 'r'  
+	else totalWaterVolume += (waterFlow_L_min/60)*(dt/1000); // volumen(L)=caudal(L/s)*tiempo(s)
 		
 	// Check button status. If it is low, buttonPresed = true
 	bool buttonPressed = !digitalRead(PIN_INPUT_BUTTON);	
@@ -219,11 +231,11 @@ void printMeasurements(int hum, int temp, float voltage, float waterFlow, float 
 	Serial.println(temp);
 	Serial.print("Voltage: ");
 	Serial.println(voltage);
-	Serial.print ("Caudal: "); 
-	Serial.print (waterFlow,3); 
-	Serial.print ("L/min\tVolumen: "); 
-	Serial.print (totalVolume,3); 
-	Serial.println (" L");
+	Serial.print("Caudal: "); 
+	Serial.print(waterFlow,3); 
+	Serial.print("L/min\tVolumen: "); 
+	Serial.print(totalVolume,3); 
+	Serial.println(" L");
 	Serial.print("Boton pulsado: ");
 	Serial.println(buttonPressed);
 }
@@ -247,21 +259,5 @@ void sendSMS(String text, String phone_number)
 	SerialSIM800.print((char)26);
 	delay(500);
 	
-	SerialSIM800.println();
-	
 	if(DEBUG_MODE) Serial.println("Text Sent.");
-	delay(500);
 }
-
-// Serial event on monitor serial1
-void serialEvent(){   
-	// Check if anything has been received in the SIM800
-	if (SerialSIM800.available() > 0){            
-		if(DEBUG_MODE) 
-		{
-			//Displays on the serial monitor if there's a communication from the module
-			Serial.println("Message received from SIM800: ");
-			Serial.write(SerialSIM800.readStringUntil("\n")); 
-		}
-	}
-}	
